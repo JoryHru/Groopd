fe2d4ae8fadd1608f96c886f664d44cc9032ed51
Jory Hru <joryhru@aol.com>
2019-01-15 16:33:41 -0500

    Create Groopd List VC and Contacts VC

---
+++ Groopd/Controller/GroopdListViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Groopd
+//
+//  Created by Edward Hru on 1/15/19.
+//  Copyright © 2019 Edward Hru. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class GroopdListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        // Configure the cell...
+        
+        return cell
+    }
+    
+    //MARK: Tableview Delegate Method
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+
+}
+
